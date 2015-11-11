6742d4689ab637f55146e5f54f6db971f10146be
Chris Warburton <chriswarbo@gmail.com>
2015-11-11 14:40:28 +0000

    Moved non-versioned scripts into warbo-utilities

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   buildPhase   = "";
   installPhase = ''
     mkdir -p "$out/bin"
-    for DIR in svn system web
+    for DIR in svn system web git
     do
         cp "$DIR/"* "$out/bin/"
     done
