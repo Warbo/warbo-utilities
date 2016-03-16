5b07679aba6f18122cdfd5c9f58e7fc0e7181952
Chris Warburton <chriswarbo@gmail.com>
2016-03-16 13:48:10 +0000

    Quote args in trace

--- profiling/proc_trace.sh
+++ profiling/proc_trace.sh
@@ -5,6 +5,6 @@
 #  -e trace=process  Only log process-related events (creation, exit, ...)
 #  -ttt              Give absolute UNIX timestamps for each event
 #  -q                Don't log tracer attaches/detaches
-strace -f -e trace=process -ttt -q $@
+strace -f -e trace=process -ttt -q "$@"
 
 # NOTE: Trace is logged to stderr, so use 2> to save it
