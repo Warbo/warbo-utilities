e440233acc6d51b1df0bab870814bd6cd0acfc25
Chris Warburton <chriswarbo@gmail.com>
2016-04-24 16:19:32 +0100

    Check metal archives results

--- music/check_artist_names.sh
+++ music/check_artist_names.sh
@@ -1,17 +1,59 @@
 #!/usr/bin/env bash
 set -e
 
-# Check each directory at the artist level against MusicBrainz
+# Check each directory at the artist level against external metadata databases
+
+function assertDir {
+    [[ -d "$1" ]] || {
+        echo "Error: '$1' isn't a directory" 1>&2
+        return 1
+    }
+    return 0
+}
+
+function metalArchiveFor {
+    CACHED="$CACHE_DIR/$INIT/$1"
+    if [[ -f "$CACHED" ]]
+    then
+        echo "Using cache for '$1'" 1>&2
+    else
+        echo "Searching for '$1' on metal-archives.com" 1>&2
+        sleep 1
+        curl --get --data-urlencode "field=name"      \
+             --data-urlencode "query=$1" \
+             "http://www.metal-archives.com/search/ajax-band-search/" > "$CACHED"
+    fi
+    [[ -f "$CACHED" ]] || {
+        echo "Couldn't find metal-archives data for '$1'" 1>&2
+        return 1
+    }
+    echo "$CACHED"
+}
+
+function haveMetalArchive {
+    ARCHIVE=$(metalArchiveFor "$1") || return 1
+    MATCHES=$(jq '.iTotalRecords' < "$ARCHIVE")
+    if [[ "$MATCHES" -eq 1 ]]
+    then
+        echo "Found '$ARTIST' on metal-archives.com" 1>&2
+        return 0
+    fi
+
+    echo "Found '$MATCHES' results for '$ARTIST'" 1>&2
+    return 1
+}
+
+function checkArtistDir {
+    ARTIST=$(basename "$1")
+    haveMetalArchive "$ARTIST"
+}
 
 CACHE_DIR="$PWD/.artist_name_cache"
 mkdir -p "$CACHE_DIR"
 
 for D1 in Music/Commercial/*
 do
-    [[ -d "$D1" ]] || {
-        echo "Error: '$D1' isn't a directory" 1>&2
-        continue
-    }
+    assertDir "$D1" || continue
     INIT=$(basename "$D1")
     mkdir -p "$CACHE_DIR/$INIT"
 
@@ -19,22 +61,10 @@ do
 
     for D2 in "$D1/"*
     do
-        [[ -d "$D2" ]] || {
-            echo "Error: '$D2' isn't a directory" 1>&2
-            continue
-        }
+        assertDir "$D2" || continue
 
-        ARTIST=$(basename "$D2")
-        CACHED="$CACHE_DIR/$INIT/$ARTIST"
-        if [[ -f "$CACHED" ]]
-        then
-            echo "Using cache for '$ARTIST'" 1>&2
-        else
-            echo "Searching for '$ARTIST' on metal-archives.com" 1>&2
-            sleep 1
-            curl --get --data-urlencode "field=name"      \
-                       --data-urlencode "query=$ARTIST" \
-                 "http://www.metal-archives.com/search/ajax-band-search/" > "$CACHED"
-        fi
+        checkArtistDir "$D2" || {
+            echo "Error: Failed to find artist for '$D2'" 1>&2
+        }
     done
 done
