3d8036e661e7fc35fa2b2187915c73795e2d3788
Chris Warburton <chriswarbo@gmail.com>
2016-04-24 20:06:52 +0100

    Move directories

--- music/no_dodgy_names.sh
+++ music/no_dodgy_names.sh
@@ -19,4 +19,9 @@ do
     echo "rmdir '$ESCAPED'"
 done
 
-find Music/Commercial -name '*magnatune.com*'
+find Music/Commercial -type d -name '*magnatune.com*' | while read -r DIR
+do
+     SRC=$(echo "$DIR" | esc)
+    DEST=$(echo "$DIR" | sed -e 's@ (PREVIEW_ buy it at www.magnatune.com)@@g' | esc)
+    echo "mv -v '$SRC' '$DEST'"
+done
