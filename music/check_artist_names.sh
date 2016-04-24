d8748d3be53cbb535f2e889d7dab605efc9ca3c0
Chris Warburton <chriswarbo@gmail.com>
2016-04-24 13:42:41 +0100

    Debugging

--- music/check_artist_names.sh
+++ music/check_artist_names.sh
@@ -8,8 +8,6 @@ mkdir -p "$CACHE_DIR"
 
 for D1 in Music/Commercial/*
 do
-    echo "$D1"
-    continue
     [[ -d "$D1" ]] || {
         echo "Error: '$D1' isn't a directory" 1>&2
         continue
@@ -17,8 +15,16 @@ do
     INIT=$(basename "$D1")
     mkdir -p "$CACHE_DIR/$INIT"
 
+    echo "Checking $INIT..." 1>&2
+
     for D2 in "$D1"
     do
-        true
+        echo "$D2"
+        break
+        [[ -d "$D2" ]] || {
+            echo "Error: '$D2' isn't a directory" 1>&2
+            continue
+        }
     done
+    break
 done
