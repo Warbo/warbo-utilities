18c72fd4bbbcc72175824dc39abd56a050babea4
Chris Warburton <chriswarbo@gmail.com>
2016-04-24 19:45:45 +0100

    Echo command for dodgy names

--- music/no_dodgy_names.sh
+++ music/no_dodgy_names.sh
@@ -2,4 +2,21 @@
 
 set -e
 
-find Music/Commercial -name 'http*'
+function esc {
+    sed -e "s@'@'\\''@g"
+}
+
+find Music/Commercial -name 'http___music.download.com' | while read -r DIR
+do
+    PARENT=$(dirname "$DIR")
+    for ENTRY in "$DIR"/*
+    do
+         SRC=$(echo "$ENTRY"  | esc)
+        DEST=$(echo "$PARENT" | esc)
+        echo "mv '$SRC' '$DEST'"
+    done
+    ESCAPED=$(echo "$DIR" | esc)
+    echo "rmdir '$ESCAPED'"
+done
+
+find Music/Commercial -name '*magnatune.com*'
