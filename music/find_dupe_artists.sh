601a6ef2c8523195c792eb1f0cd93485d79832cc
Chris Warburton <chriswarbo@gmail.com>
2016-05-07 15:03:06 +0100

    Ingore known nondupes

--- music/find_dupe_artists.sh
+++ music/find_dupe_artists.sh
@@ -6,8 +6,16 @@ BASE=$(dirname "$(readlink -f "$0")")
 # it collides with another, eg. "AC-DC" and "ACDC" both become "acdc"
 echo "Looking for possible dupes in Music/Commercial" 1>&2
 
+ALLOWED=".allowed_artist_dupes"
+[[ -e "$ALLOWED" ]] || {
+    echo "Making empty '$ALLOWED'" 1>&2
+    touch "$ALLOWED"
+}
+
 for INIT in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 do
     echo "$INIT" 1>&2
-    ls "Music/Commercial/$INIT/" | "$BASE/list_dupe_guesses.sh"
+    ls "Music/Commercial/$INIT/"    |
+       "$BASE/list_dupe_guesses.sh" |  # "name1\tname2" for potential dupe names
+       grep -F -x -v -f "$ALLOWED"     # Ignore known non-dupes
 done
