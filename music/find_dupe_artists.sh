619374ed3ef6883da41fb79896cfa631a35c784a
Chris Warburton <chriswarbo@gmail.com>
2016-05-07 14:45:18 +0100

    Messages to stderr

--- music/find_dupe_artists.sh
+++ music/find_dupe_artists.sh
@@ -4,10 +4,10 @@ BASE=$(dirname "$(readlink -f "$0")")
 
 # For each directory at the artist level, we 'simplify' the name to see if
 # it collides with another, eg. "AC-DC" and "ACDC" both become "acdc"
-echo "Looking for possible dupes in Music/Commercial"
+echo "Looking for possible dupes in Music/Commercial" 1>&2
 
 for INIT in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 do
-    echo "$INIT"
+    echo "$INIT" 1>&2
     ls "Music/Commercial/$INIT/" | "$BASE/list_dupe_guesses.sh"
 done
