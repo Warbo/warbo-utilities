46f79db6446f246d565818be8754cc10d2dbc30c
Chris Warburton <chriswarbo@gmail.com>
2016-05-25 13:10:28 +0100

    Update move_music.sh

--- music/move_music.sh
+++ music/move_music.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 shopt -s nullglob
 
 BASE=$(dirname "$(readlink -f "$0")")
 
-#"$BASE/move_contents.sh"
-#"$BASE/delete_crap.sh"
-#"$BASE/remove_empties.sh"
-#"$BASE/no_discs.sh"
-#"$BASE/free_out_of_commercial.sh"
-#"$BASE/normalise_whitespace.sh"
+"$BASE/delete_crap.sh"
+"$BASE/remove_empties.sh"
+"$BASE/no_discs.sh"
+"$BASE/free_out_of_commercial.sh"
+"$BASE/no_dodgy_names.sh"
+"$BASE/find_full_albums.sh"
+"$BASE/normalise_whitespace.sh"
