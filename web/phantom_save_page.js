7a18f8e4017ec12651687cb7fc47e576148ad2ee
Chris Warburton <chriswarbo@gmail.com>
2016-05-30 15:52:28 +0100

    Use firefox automation

--- web/phantom_save_page.js
+++ web/phantom_save_page.js
@@ -6,12 +6,36 @@
 var page   = require('webpage').create();
 var system = require('system');
 
+// Make ourselves look more like a browser than a scraper
+page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
+page.viewportSize = { width: 1024, height: 768 };
+
+// Add an error handler, since sites may have dodgy code
+// Taken from http://stackoverflow.com/a/19538646/884682
+handler = function(msg, trace) {
+    var msgStack = ['ERROR: ' + msg];
+    if (trace && trace.length) {
+        msgStack.push('TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+        });
+    }
+
+    system.stderr.write(msgStack.join('\n'));
+};
+
+page.onError    = handler;
+phantom.onError = handler;
+
 page.open(system.args[1], function() {
     page.evaluate(function() {
     });
 });
 
 page.onLoadFinished = function() {
-    system.stdout.write(page.content);
-    phantom.exit(0);
+    setTimeout(function() {
+        system.stdout.write(page.content);
+        page.render('debug.png');
+        phantom.exit(0);
+    }, 2000);
 };
