0216b8a40f9fca472d96d2655cb49096fd4ed04d
Chris Warburton <chriswarbo@gmail.com>
2016-06-08 19:55:47 +0100

    Fake tty

--- web/cw_update.sh
+++ web/cw_update.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-echo '' | sudo -S ./su_update.sh
+expect -c "spawn -noecho sh -c {
+     exec sudo ./su_update.sh >&4 2>&5 <&6 4>&- 5>&- 6<&-}
+ exit [lindex [wait] 3]" 4>&1 5>&2 6<&0
