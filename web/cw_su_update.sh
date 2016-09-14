9db534f09efa77fd55fc97bd8e9dba362f63c473
Chris Warburton <chriswarbo@gmail.com>
2016-09-14 20:01:54 +0100

    Only make link if needed

--- web/cw_su_update.sh
+++ web/cw_su_update.sh
@@ -3,7 +3,8 @@
 if [[ -d rendered ]]
 then
     D=$(date +%s)
-    (cd rendered && ln -s /opt/git git)
+    (pushd rendered > /dev/null;
+     [[ -e git ]] || ln -s /opt/git git)
     mv -v /var/www/html "old/$D" && mv -v rendered /var/www/html
 else
     echo "No 'rendered' directory" 1>&2
