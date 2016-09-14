47d7f256903655b7286b2e07ad004cbd389a93f4
Chris Warburton <chriswarbo@gmail.com>
2016-09-14 20:33:20 +0100

    Allow broken symlinks

--- web/cw_su_update.sh
+++ web/cw_su_update.sh
@@ -4,7 +4,7 @@ if [[ -d rendered ]]
 then
     D=$(date +%s)
     (pushd rendered > /dev/null;
-     [[ -e git ]] || ln -s /opt/git git)
+     [[ -h git ]] || ln -s /opt/git git)
     mv -v /var/www/html "old/$D" && mv -v rendered /var/www/html
 else
     echo "No 'rendered' directory" 1>&2
