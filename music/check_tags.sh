d663446f86a714c4b989e8be6ae1334255f9b405
Chris Warburton <chriswarbo@gmail.com>
2016-09-14 18:12:32 +0100

    Added check_tags

---
+++ music/check_tags.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function artistOf {
+    A_DIR=$(echo "$1" | grep -o "Music/Commercial/[^/]*/[^/]*")
+    basename "$A_DIR"
+}
+
+function albumOf {
+    A_DIR=$(echo "$1" | grep -o "Music/Commercial/[^/]*/[^/]*/[^/]*")
+    basename "$A_DIR"
+}
+
+function checkTag {
+    GOT=$(echo "$DATA" | grep "^$1" | cut -d ' ' -f 2-)
+    [[ "x$GOT" = "x$2" ]] || return 1
+}
+
+function checkMp3s {
+    command -v id3v2 > /dev/null || {
+        echo "Not checking MP3 tags since id3v2 not found" 1>&2
+        return
+    }
+    while read -r F
+    do
+        ARTIST=$(artistOf "$F")
+         ALBUM=$( albumOf "$F")
+
+        DATA=$(id3v2 -R "$F")
+        HAS=$(checkTag "TPE2" "$ARTIST") || {
+            echo "$F has artist '$HAS', should be '$ARTIST'"
+        }
+    done < <(find Music -type f -iname "*.mp3" | head)
+}
