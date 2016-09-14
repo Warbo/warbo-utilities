3d24e4fa9a1fa6cb17a19c3e563d0ef7b647c865
Chris Warburton <chriswarbo@gmail.com>
2016-09-15 00:25:49 +0100

    Improved check_tags.sh

--- music/check_tags.sh
+++ music/check_tags.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+BASE=$(dirname "$(readlink -f "$0")")
+
+function esc {
+    "$BASE/esc.sh"
+}
+
+function initOf {
+    I_DIR=$(echo "$1" | grep -o "Music/Commercial/[^/]*")
+    basename "$I_DIR"
+}
+
 function artistOf {
     A_DIR=$(echo "$1" | grep -o "Music/Commercial/[^/]*/[^/]*")
     basename "$A_DIR"
@@ -11,25 +22,74 @@ function albumOf {
 }
 
 function checkTag {
-    GOT=$(echo "$DATA" | grep "^$1" | cut -d ' ' -f 2-)
-    [[ "x$GOT" = "x$2" ]] || return 1
+    GOT=$(echo "$DATA" | grep "^$1=" | cut -d '=' -f 2-)
+    [[ "x$GOT" = "x$2" ]] || {
+        echo "$GOT"
+        return 1
+    }
+}
+
+function tagsFor {
+    # Artist
+    ART_ESC=$(echo "$1" | esc)
+
+    echo -e "TPE1\t$1\t--artist='$ART_ESC'"
+    echo -e "TPE2\t$1"
+
+    # Album
+    ALB_ESC=$(echo "$2" | esc)
+    echo -e "TALB\t$2\t--album='$ALB_ESC'"
 }
 
 function checkMp3s {
-    command -v id3v2 > /dev/null || {
-        echo "Not checking MP3 tags since id3v2 not found" 1>&2
+    command -v mid3v2 > /dev/null || {
+        echo "Not checking MP3 tags since mid3v2 not found" 1>&2
         return
     }
     while read -r F
     do
+          INIT=$(initOf   "$F")
         ARTIST=$(artistOf "$F")
-         ALBUM=$( albumOf "$F")
+         ALBUM=$(albumOf  "$F")
 
-        DATA=$(id3v2 -R "$F")
-        HAS=$(checkTag "TPE2" "$ARTIST") || {
-            echo "$F has artist '$HAS', should be '$ARTIST'"
+        [[ -d "Music/Commercial/$INIT"                ]] || {
+            echo "Couldn't get initial for '$F', skipping" 1>&2
+            continue
         }
-    done < <(find Music/Commercial -type f -iname "*.mp3" | head)
+
+        [[ -d "Music/Commercial/$INIT/$ARTIST"        ]] || {
+            echo "Couldn't get artist for '$F', skipping" 1>&2
+            continue
+        }
+
+        [[ -d "Music/Commercial/$INIT/$ARTIST/$ALBUM" ]] || {
+            echo "Couldn't get album for '$F'" 1>&2
+            ALBUM="NONE"
+        }
+
+         F_ESC=$(echo "$F" | esc)
+
+        # Normalise the ID3 data, so it's all available as v2, and read it in
+        mid3v2 --convert "$F"
+        DATA=$(mid3v2 --list "$F")
+
+        while read -r LINE
+        do
+            FIELD=$(echo "$LINE" | cut -f 1)
+              VAL=$(echo "$LINE" | cut -f 2)
+              FIX=$(echo "$LINE" | cut -f 3)
+
+            if [[ "x$VAL" = "xNONE" ]]
+            then
+                continue
+            fi
+
+            HAS=$(checkTag "$FIELD" "$VAL") || {
+                echo "$F has '$FIELD' of '$HAS', should be '$VAL'"
+                echo "mid3v2 $FIX '$F'"
+            }
+        done < <(tagsFor "$ARTIST" "$ALBUM")
+    done < <(find Music/Commercial -type f -iname "*.mp3")
 }
 
 checkMp3s
