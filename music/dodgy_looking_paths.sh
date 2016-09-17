6c8aad153d6d9929dafbe85816260cb331e093c5
Chris Warburton <chriswarbo@gmail.com>
2016-09-17 13:33:57 +0100

    Look for known dodgy paths

--- music/dodgy_looking_paths.sh
+++ music/dodgy_looking_paths.sh
@@ -2,6 +2,40 @@
 
 BASE=$(dirname "$(readlink -f "$0")")
 
+# Quick checks, where we know what to look for
+
+for ARTIST in Music/Commercial/*/*
+do
+    NAME=$(basename "$ARTIST")
+
+    if echo "$NAME" | grep '^.\.\(.\.\)*.$' > /dev/null
+    then
+        echo "'$NAME' should probably end in a '.'"
+    fi
+
+    if echo "$NAME" | grep ', The' > /dev/null
+    then
+        echo "'$NAME' should probably be 'The ...'"
+    fi
+done
+
+find Music/Commercial/ -maxdepth 2 | while read -r P
+do
+    NAME=$(basename "$P")
+    for ARTIST in Blue\ Oyster\ Cult  \
+                      Blue\ Oeyster\ Cult \
+                      Motvrhead           \
+                      Motorhead           \
+                      Motoerhead
+    do
+        if [[ "x$NAME" = "x$ARTIST" ]]
+        then
+            echo "Found badly named '$NAME' directory"
+        fi
+    done
+done
+
+# More thorough checks, for each file and directory
 for I in Music/Commercial/*
 do
     I_ESC=$(echo "$I" | "$BASE/esc.sh")
