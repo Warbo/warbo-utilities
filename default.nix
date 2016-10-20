4bcb7cb991865c4696b7d4241760dacc2fa0a689
Chris Warburton <chriswarbo@gmail.com>
2016-10-20 11:51:51 +0100

    Move scripts into Nix derivation

--- default.nix
+++ default.nix
@@ -1,4 +1,20 @@
-{ jsbeautifier, python, phantomjs, stdenv, xdotool, xidel, xsel, xvfb_run }:
+{ alsaUtils, bash, jsbeautifier, lib, pidgin, python, phantomjs, stdenv,
+  writeScript, xdotool, xidel, xsel, xvfb_run }:
+
+with builtins;
+with lib;
+with rec {
+  system = rec {
+    honk = writeScript "honk" ''
+      #!${bash}/bin/bash
+      ${alsaUtils}/bin/amixer sset Master unmute > /dev/null
+      ${alsaUtils}/bin/aplay ${pidgin}/share/sounds/purple/alert.wav > /dev/null 2>&1
+    '';
+  };
+
+  mkCmd = name: script: ''cp "${script}" "$out/bin/${name}"'';
+  cmds  = concatMap (set: attrValues (mapAttrs mkCmd set)) [ system ];
+};
 
 stdenv.mkDerivation {
   name = "warbo-utilities";
@@ -14,12 +30,12 @@ stdenv.mkDerivation {
     xsel
   ];
 
-  buildPhase   = "";
   installPhase = ''
     mkdir -p "$out/bin"
     for DIR in svn system web git development testing docs
     do
-        cp "$DIR/"* "$out/bin/"
+      cp "$DIR/"* "$out/bin/"
     done
+    ${concatStringsSep "\n" cmds}
   '';
 }
