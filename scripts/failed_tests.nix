4a59dbafd0bc6b0aac6accaeaab627cf5d4fe2e8
Chris Warburton <chriswarbo@gmail.com>
2016-11-22 12:37:54 +0000

    Nixify failed_tests

--- scripts/failed_tests.nix
+++ scripts/failed_tests.nix
@@ -1,73 +1,87 @@
-#!/usr/bin/env bash
-shopt -s nullglob
+{ bash, findutils, jq, makeWrapper, runCommand, writeScript }:
 
-D="$HOME/System/Tests"
+with {
 
-function count {
-    find "$D/$1" -type f -o -type l | wc -l
-}
+script = writeScript "failed_tests" ''
+  #!${bash}/bin/bash
+  shopt -s nullglob
 
-     ATTRS=$("$D/helpers/tests.sh")
- ATTRCOUNT=$(echo "$ATTRS" | jq 'length')
-ATTRPASSES=$(echo "$ATTRS" | jq 'map(select(.pass)) | length')
- NOTPASSED=$(echo "$ATTRS" | jq -r 'to_entries | map(select(.value.pass | not)) | .[] | .key')
+  D="$HOME/System/Tests"
 
-SCRIPTS=$(count scripts/)
-   PASS=$(count results/pass/)
-  CHECK=$(count results/check/)
-RUNNING=$(count results/running/)
+  function count {
+      find "$D/$1" -type f -o -type l | wc -l
+  }
 
-    ALL=$(( SCRIPTS + ATTRCOUNT ))
-SUCCESS=$(( PASS + CHECK + ATTRPASSES ))
+       ATTRS=$("$D/helpers/tests.sh")
+   ATTRCOUNT=$(echo "$ATTRS" | jq 'length')
+  ATTRPASSES=$(echo "$ATTRS" | jq 'map(select(.pass)) | length')
+   NOTPASSED=$(echo "$ATTRS" | jq -r 'to_entries | map(select(.value.pass | not)) | .[] | .key')
 
-DIFF=$(( ALL - SUCCESS ))
+  SCRIPTS=$(count scripts/)
+     PASS=$(count results/pass/)
+    CHECK=$(count results/check/)
+  RUNNING=$(count results/running/)
 
-HEX="#00FF00"
-[[ "$DIFF" -gt 0 ]] && HEX="#FF0000"
+      ALL=$(( SCRIPTS + ATTRCOUNT ))
+  SUCCESS=$(( PASS + CHECK + ATTRPASSES ))
 
-echo "<fc=${HEX}>${SUCCESS}/${ALL}</fc>"
+  DIFF=$(( ALL - SUCCESS ))
 
-[[ "x$1" = "xshow" ]] || exit 0
+  HEX="#00FF00"
+  [[ "$DIFF" -gt 0 ]] && HEX="#FF0000"
 
-echo "PASS:    $PASS"
-echo "CHECK:   $CHECK"
-echo "SUCCESS: $SUCCESS"
-echo "RUNNING: $RUNNING"
-echo "ALL:     $ALL"
+  printf '<fc=%s>%s/%s</fc>\n' "$HEX" "$SUCCESS" "$ALL"
 
-echo "Passed:"
-ls "$D/results/pass/"
-echo ""
+  [[ "x$1" = "xshow" ]] || exit 0
 
-echo "To check:"
-ls "$D/results/check/"
-echo ""
+  echo "PASS:    $PASS"
+  echo "CHECK:   $CHECK"
+  echo "SUCCESS: $SUCCESS"
+  echo "RUNNING: $RUNNING"
+  echo "ALL:     $ALL"
 
-echo "Not run:"
-for TEST in "$D/scripts"/*
-do
-    NAME=$(basename "$TEST")
-    [[ -e "$D/results/stdout/$NAME" ]] || echo "$NAME"
-done
-while read -r NAME
-do
-    [[ -e "$D/results/stdout/$NAME" ]] || echo "$NAME"
-done < <(echo "$NOTPASSED")
-echo ""
+  echo "Passed:"
+  ls "$D/results/pass/"
+  echo ""
 
-echo "Failures:"
-for TEST in "$D/scripts"/*
-do
-    NAME=$(basename "$TEST")
-    [[ -e "$D/results/stdout/$NAME" ]] || continue
-    [[ -e "$D/results/pass/$NAME"   ]] && continue
-    [[ -e "$D/results/check/$NAME"  ]] && continue
-    echo "$NAME"
-done
-while read -r NAME
-do
-    [[ -e "$D/results/stdout/$NAME" ]] || continue
-    [[ -e "$D/results/pass/$NAME"   ]] && continue
-    [[ -e "$D/results/check/$NAME"  ]] && continue
-    echo "$NAME"
-done < <(echo "$NOTPASSED")
+  echo "To check:"
+  ls "$D/results/check/"
+  echo ""
+
+  echo "Not run:"
+  for TEST in "$D/scripts"/*
+  do
+      NAME=$(basename "$TEST")
+      [[ -e "$D/results/stdout/$NAME" ]] || echo "$NAME"
+  done
+  while read -r NAME
+  do
+      [[ -e "$D/results/stdout/$NAME" ]] || echo "$NAME"
+  done < <(echo "$NOTPASSED")
+  echo ""
+
+  echo "Failures:"
+  for TEST in "$D/scripts"/*
+  do
+      NAME=$(basename "$TEST")
+      [[ -e "$D/results/stdout/$NAME" ]] || continue
+      [[ -e "$D/results/pass/$NAME"   ]] && continue
+      [[ -e "$D/results/check/$NAME"  ]] && continue
+      echo "$NAME"
+  done
+  while read -r NAME
+  do
+      [[ -e "$D/results/stdout/$NAME" ]] || continue
+      [[ -e "$D/results/pass/$NAME"   ]] && continue
+      [[ -e "$D/results/check/$NAME"  ]] && continue
+      echo "$NAME"
+  done < <(echo "$NOTPASSED")
+'';
+
+};
+
+runCommand "failed_tests_wrapped" { buildInputs = [ makeWrapper ]; } ''
+  #!${bash}/bin/bash
+  makeWrapper "${script}" "$out" --prefix PATH : "${jq}/bin" \
+                                 --prefix PATH : "${findutils}/bin" \
+''
