118adfe680e2c6435444a7371f4a4af8300e6954
Chris Warburton <chriswarbo@gmail.com>
2017-02-11 20:51:24 +0000

    Bump sleeps

--- scripts/getalluc.nix
+++ scripts/getalluc.nix
@@ -77,28 +77,28 @@ with (rec {
                             -new-instance      \
                             "$URL" 1>&2 &
          FF_PID="$!"
-         sleep 5
+         sleep 10
 
          echo "Skipping safe mode prompt" 1>&2
          xdotool key --clearmodifiers Return
-         sleep 10
+         sleep 15
 
          echo "Opening Web console" 1>&2
          xdotool key ctrl+shift+K
-         sleep 5
+         sleep 10
 
          echo "Extracting body HTML" 1>&2
 
          # shellcheck disable=SC2016
          xdotool type 'window.prompt("Copy to clipboard: Ctrl+C, Enter", document.body.innerHTML);'
 
-         sleep 1
+         sleep 5
          xdotool key --clearmodifiers Return
-         sleep 1
+         sleep 5
 
          echo "Copying content" 1>&2
          xdotool key ctrl+c
-         sleep 1
+         sleep 5
 
          echo "Pasting content" 1>&2
          xsel --clipboard
