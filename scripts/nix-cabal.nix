3940e1ba6367cf2485e8ef2234822a0c805bfa7e
Chris Warburton <chriswarbo@gmail.com>
2017-05-10 22:25:31 +0100

    Use global cabal* packages, rather than those from haskellPackages

--- scripts/nix-cabal.nix
+++ scripts/nix-cabal.nix
@@ -1,16 +1,16 @@
-{ bash, haskellPackages, makeWrapper, runCommand, writeScript }:
+{ bash, cabal-install, cabal2nix, makeWrapper, runCommand, writeScript }:
 
 runCommand "wrap-nix-cabal"
   {
     buildInputs = [ makeWrapper ];
     raw         = writeScript "nix-cabal" ''
-                    #!${bash}/bin/bash
-                    exec nix-shell -E "$(cabal2nix --shell ./.)" --run "cabal $@"
-                  '';
+      #!${bash}/bin/bash
+      exec nix-shell -E "$(cabal2nix --shell ./.)" --run "cabal $@"
+    '';
   }
   ''
     #!${bash}/bin/bash
     makeWrapper "$raw" "$out" \
-      --prefix PATH : "${haskellPackages.cabal-install}/bin" \
-      --prefix PATH : "${haskellPackages.cabal2nix}/bin"
+      --prefix PATH : "${cabal-install}/bin" \
+      --prefix PATH : "${cabal2nix}/bin"
   ''
