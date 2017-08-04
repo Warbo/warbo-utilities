2f695fb5d37da9c173992f86aa3cee59a0f8466d
Chris Warburton <chriswarbo@gmail.com>
2017-08-04 18:00:43 +0100

    Use wrap in hsConfig

--- scripts/hsConfig.nix
+++ scripts/hsConfig.nix
@@ -1,26 +1,18 @@
-{ bash, cabal2nix, makeWrapper, nix, stdenv, writeScript }:
-with builtins;
-with rec {
-  expr = writeScript "hsConfigExpr.nix" ''
-    with import <nixpkgs> {};
-    { c2n }: (tincify (haskellPackages.callPackage c2n {}) {}).env
-  '';
+{ cabal2nix, pkgconfig, wrap }:
 
-  raw  = writeScript "hsConfig" ''
-    #!${bash}/bin/bash -p
-    CMD="cabal configure -v --enable-tests --enable-benchmarks $*"
-    nix-shell --show-trace --arg c2n "$(cabal2nix ./.)" -E "import ${expr}" \
-              --run "$CMD"
-  '';
-};
-stdenv.mkDerivation {
-  name         = "hsConfig";
-  buildInputs  = [ makeWrapper ];
-  buildCommand = ''
+with builtins;
+wrap {
+  name  = "hsConfig";
+  paths = [ cabal2nix pkgconfig ];
+  vars  = {
+    command = "cabal configure -v --enable-tests --enable-benchmarks";
+    expr    = ''with import <nixpkgs> {}; { c2n }:
+                (tincify (haskellPackages.callPackage c2n {}) {}).env'';
+  };
+  script = ''
+    #!/usr/bin/env bash
     set -e
-    makeWrapper "${raw}" "$out" --prefix PATH :     "${cabal2nix}/bin" \
-                                --prefix PATH :     "${nix.out}/bin"   \
-                                --set    NIX_REMOTE "daemon"           \
-                                --set    NIX_PATH   "${getEnv "NIX_PATH"}"
+    C2N=$(cabal2nix ./.)
+    nix-shell --show-trace --arg c2n "$C2N" -E "$expr" --run "$command $*"
   '';
 }
