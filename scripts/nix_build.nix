241dfaca368853a6035dd7b6e2535b09d4ad42e7
Chris Warburton <chriswarbo@gmail.com>
2017-09-12 13:43:22 +0100

    Added nix_build

---
+++ scripts/nix_build.nix
@@ -0,0 +1,30 @@
+{ fail, wrap, writeScript }:
+
+wrap {
+  name   = "nix_build";
+  paths  = [ fail ];
+  vars   = {
+    expr = writeScript "nix_build_expr.nix" ''
+      with builtins;
+      with import <nixpkgs> {};
+      with rec {
+        src = getEnv "JOB";
+        job = if typeOf (import src) == "lambda"
+                 then callPackage src {}
+                 else import src;
+      };
+      withDeps (allDrvsIn job) nothing
+    '';
+  };
+  script = ''
+    #!/usr/bin/env bash
+    JOB="$PWD/release.nix"
+    [[ -z "$1" ]] || JOB=$(readlink -f "$1")
+    export JOB
+
+    [[ -e "$JOB" ]] || fail "Couldn't find '$JOB'; maybe give an argument?"
+
+    echo "Building derivations from '$JOB'" 1>&2
+    nix-build --show-trace --no-out-link -E 'import (builtins.getEnv "expr")'
+  '';
+}
