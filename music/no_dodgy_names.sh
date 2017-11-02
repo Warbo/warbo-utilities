842d58960f09c913f6803ab447ccc97f276d52a4
Chris Warburton <chriswarbo@gmail.com>
2017-11-02 17:27:08 +0000

    Look for tmp_ in no_dodgy_names

--- music/no_dodgy_names.sh
+++ music/no_dodgy_names.sh
@@ -32,3 +32,17 @@ do
     DEST=$(echo "$F" | sed -e 's@ (PREVIEW_ buy it at www.magnatune.com)@@g' | esc)
     echo "mv -v '$SRC' '$DEST'"
 done
+
+find Music/Commercial -type f -name 'tmp_*' | while read -r F
+do
+    echo "Name '$F' looks like a dupe" 1>&2
+    OTHER=$(basename "$F" | cut -d '_' -f 2-)
+      DIR=$(dirname "$F")
+      ESC=$(echo "$F" | esc)
+    if [[ -e "$DIR/$OTHER" ]]
+    then
+        echo "rm -v '$ESC'"
+    else
+        echo "No equivalent '$OTHER' found in '$DIR' though" 1>&2
+    fi
+done
