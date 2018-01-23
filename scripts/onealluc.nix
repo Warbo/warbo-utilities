6d1656ef7563cd501f387438351a7f3323861d4c
Chris Warburton <chriswarbo@gmail.com>
2018-01-23 15:18:21 +0000

    Simplify onealluc using wrap

--- scripts/onealluc.nix
+++ scripts/onealluc.nix
@@ -1,30 +1,27 @@
-{ expect, makeWrapper, runCommand, writeScript }:
+{ expect, wrap }:
 
-runCommand "onealluc"
-  {
-    buildInputs = [ makeWrapper ];
-    raw = writeScript "onealluc" ''
-      #!/usr/bin/env expect
-      # Default timeout is 10 seconds; -1 disables timeout
-      set timeout -1
+wrap {
+  name   = "onealluc";
+  paths  = [ expect ];
+  script = ''
+    #!/usr/bin/env expect
+    # Default timeout is 10 seconds; -1 disables timeout
+    set timeout -1
 
-      # Run getalluc with all args, like bash $@
-      set arguments [lrange $argv 0 end]
-      spawn getalluc {*}$arguments
+    # Run getalluc with all args, like bash $@
+    set arguments [lrange $argv 0 end]
+    spawn getalluc {*}$arguments
 
-      # Quit after the first "inDir ..."
-      expect {
-        eof {
-          send_user "\nNo results\n"
-          exit 0
-        }
-        "inDir" {
-          exit 0
-        }
+    # Quit after the first "inDir ..."
+    expect {
+      eof {
+        send_user "\nNo results\n"
+        exit 0
       }
-      close
-    '';
-  }
-  ''
-    makeWrapper "$raw" "$out" --prefix PATH : "${expect}/bin"
-  ''
+      "inDir" {
+        exit 0
+      }
+    }
+    close
+  '';
+}
