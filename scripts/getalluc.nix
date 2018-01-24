7fa38b6e184f41f84638601436a90a2fc9544d42
Chris Warburton <chriswarbo@gmail.com>
2018-01-24 16:09:43 +0000

    Throw away phantomjs scraper
    It's much faster than loading up Firefox in XVFB, but it also means we're having
    to manage two separate tools for doing the same job (load a page, including JS,
    wait a few seconds and dump out the HTML).
    
    Since phantomjs seems to be getting blocked by some hosts, we need the Firefox
    scraper. Hence we might as well use it for everything that needs JS (we can
    still use curl and wget if we don't care about blockers or JS).

--- scripts/getalluc.nix
+++ scripts/getalluc.nix
@@ -1,4 +1,4 @@
-{ bash, curl, lib, phantomjs, runCommand, vidsfrompage, wget, wrap, writeScript,
+{ bash, curl, ff, lib, runCommand, vidsfrompage, wget, wrap, writeScript,
   withDeps, xidel }:
 
 with builtins;
@@ -6,69 +6,10 @@ with lib;
 with rec {
   SITE = "http://www.alluc.ee";
 
-  phantom_save_page = writeScript "phantom_save_page.js" ''
-    // Use PhantomJS to load the URL given as a commandline argument, and write
-    // the HTML to stdout. We wait for the page to finish loading before writing
-    // it out, which means we see the result of any on-page-load Javascript,
-    // which we wouldn't get with something like wget or curl.
-
-    var page   = require('webpage').create();
-    var system = require('system');
-
-    // Make ourselves look more like a browser than a scraper
-    page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
-    page.viewportSize = { width: 1024, height: 768 };
-
-    // Add an error handler, since sites may have dodgy code
-    // Taken from http://stackoverflow.com/a/19538646/884682
-    handler = function(msg, trace) {
-      var msgStack = ['ERROR: ' + msg];
-      if (trace && trace.length) {
-        msgStack.push('TRACE:');
-        trace.forEach(function(t) {
-          msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ""));
-        });
-      }
-
-      system.stderr.write(msgStack.join('\n'));
-    };
-
-    page.onError    = handler;
-    phantom.onError = handler;
-
-    page.open(system.args[1], function() {
-      page.evaluate(function() {
-      });
-    });
-
-    page.onLoadFinished = function() {
-      setTimeout(function() {
-        system.stdout.write(page.content);
-        phantom.exit(0);
-      }, 2000);
-    };
-  '';
-
-  download_page = wrap {
-    name   = "download_page";
-    paths  = [ bash phantomjs ];
-    vars   = { inherit phantom_save_page; };
-    script =  ''
-      #!/usr/bin/env bash
-
-      [[ -n "$1" ]] || {
-        echo "download_page needs a URL as argument" 1>&2
-        exit 1
-      }
-
-      phantomjs "$phantom_save_page" "$1"
-    '';
-  };
-
   getalluc = wrap {
     name   = "getalluc";
     paths  = [ bash wget xidel ];
-    vars   = { inherit download_page SITE vidsfrompage; };
+    vars   = { inherit ff SITE vidsfrompage; };
     script = ''
       #!/usr/bin/env bash
       set -e
@@ -84,17 +25,12 @@ with rec {
       Q=$(search "$@")
       URL="$SITE/stream/$Q"
 
-      function runCurl {
-        echo "Downloading '$URL'" 1>&2
-        "$download_page" "$URL"
-      }
-
       function allResults {
         xidel - -q --extract '//div[@class="title"]/a/@href'
       }
 
       function removeAds {
-        grep -v "^/source/"
+        grep -v "^/source/" | grep -v "luc\.ee#\$"
       }
 
       function prefixLinks {
@@ -103,6 +39,13 @@ with rec {
           echo "$SITE""$REL"
         done
       }
+
+      set -o pipefail
+      LINKS=$("$ff" "$URL" | allResults | removeAds | prefixLinks | uniq) ||
+        LINKS=""
+      set +o pipefail
+
+      echo -e "Search results:\n$LINKS" 1>&2
 
       while read -r LINK
       do
@@ -114,12 +57,12 @@ with rec {
                                          grep -v '\.thevideo\.me' |
                                          grep -v 'uc\.ee')
 
-        while read -r URL
+        while read -r THIS_URL
         do
-          [[ -n "$URL" ]] || continue
+          [[ -n "$THIS_URL" ]] || continue
 
-          echo "Got URL '$URL'" 1>&2
-          FIXED=$(echo "$URL" | sed -e "s/'$//g")
+          echo "Got URL '$THIS_URL'" 1>&2
+          FIXED=$(echo "$THIS_URL" | sed -e "s/'$//g")
 
           echo "Checking file type" 1>&2
           RESPONSE=$(wget --server-response --spider "$FIXED" 2>&1) || continue
@@ -135,7 +78,7 @@ with rec {
 
           [[ -z "$STOPONFIRST" ]] || exit 0
         done < <(echo "$URLS")
-      done < <(runCurl | allResults | removeAds | prefixLinks)
+      done < <(echo "$LINKS")
     '';
   };
 
