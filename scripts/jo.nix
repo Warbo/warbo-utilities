30f07ffadbbff170c0725e3ae7215b50a10f146d
Chris Warburton <chriswarbo@gmail.com>
2018-02-02 12:49:56 +0000

    Wrap 'jo' in dependencies

--- scripts/jo.nix
+++ scripts/jo.nix
@@ -1,20 +1,28 @@
-#!/usr/bin/env bash
-export XAUTHORITY=/home/chris/.Xauthority
+{ bash, openssh, wrap }:
 
-CMD="export XAUTHORITY=/home/jo/.Xauthority; x2x -west -to :0"
+wrap {
+  name   = "jo";
+  paths  = [ bash openssh ];
+  script = ''
+    #!/usr/bin/env bash
+    export XAUTHORITY=/home/chris/.Xauthority
 
-if [[ -z "$JO_HOST" ]]
-then
-   JO_HOST=debian.local
-fi
+    CMD="export XAUTHORITY=/home/jo/.Xauthority; x2x -west -to :0"
 
-if ssh jo@"$JO_HOST" true 1> /dev/null 2> /dev/null
-then
-    echo "Found $JO_HOST, starting x2x"
+    if [[ -z "$JO_HOST" ]]
+    then
+     JO_HOST=debian.local
+    fi
 
-    # shellcheck disable=SC2029
-    ssh -Y jo@"$JO_HOST" "$CMD"
-else
-    echo "Couldn't connect to $JO_HOST" 1>&2
-    exit 1
-fi
+    if ssh jo@"$JO_HOST" true 1> /dev/null 2> /dev/null
+    then
+      echo "Found $JO_HOST, starting x2x"
+
+      # shellcheck disable=SC2029
+      ssh -Y jo@"$JO_HOST" "$CMD"
+    else
+      echo "Couldn't connect to $JO_HOST" 1>&2
+      exit 1
+    fi
+  '';
+}
