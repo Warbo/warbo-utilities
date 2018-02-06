fff45fb9ef56ead75faeda2406a216f8d7d4aee0
Chris Warburton <chriswarbo@gmail.com>
2018-02-06 04:27:38 +0000

    Use fetchgit or fetchGitHashless

--- default.nix
+++ default.nix
@@ -1,30 +1,27 @@
 with builtins;
 with rec {
-  # We need a few helpers and packages from nix-config, so default to a
-  # known-good version
-  stableConfig = rec {
-    rev = "9f66c431de0d324b3f2507bea4dca5f3c2574a0e";
-    url = "https://github.com/Warbo/nix-config/archive/${rev}.tar.gz";
-  };
-
-  config =
+  # Old (< 2017) fetchgit gives different hashes, so allow override via NIX_PATH
+  options =
     with tryEval <nix-config>;
     if success
-       then (import <nixpkgs> {
-              config = import "${value}/stable.nix";
-            }).latestGit {
-              url = http://chriswarbo.net/git/nix-config.git;
-              stable = {
-                rev    = stableConfig.rev;
-                sha256 = "1x8340ns235gy76zrhf63v7hcfhw1qv630zbd7aabbcsb294hf20";
-              };
-            }
-       else fetchTarball stableConfig.url;
+       then { config = import "${value}/stable.nix"; f = "fetchGitHashless"; }
+       else { config = {};                           f = "fetchgit";         };
+
+  # We need a few helpers and packages from nix-config, so default to a
+  # known-good version
+  stableConfig =
+    getAttr (options.f)
+            (import <nixpkgs> { inherit (options) config; })
+            {
+              url    = http://chriswarbo.net/git/nix-config.git;
+              rev    = "9f66c43";
+              sha256 = "1x8340ns235gy76zrhf63v7hcfhw1qv630zbd7aabbcsb294hf20";
+            };
 
   # An awkward mix of unstable <nixpkgs> and stable nix-config. We only use this
   # to fetch other, purely stable/unstable package sets
   bootstrapPkgs = import <nixpkgs> {
-    config = import "${config}/stable.nix";
+    config = import "${stableConfig}/stable.nix";
   };
 
   # Uses stable config with stable nixpkgs
