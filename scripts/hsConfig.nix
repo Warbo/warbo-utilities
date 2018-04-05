0a75e5c9c4c07416035f544fb1cbf12a56bfc8da
Chris Warburton <chriswarbo@gmail.com>
2018-04-05 12:38:09 +0100

    Use cabal new-configure

--- scripts/hsConfig.nix
+++ scripts/hsConfig.nix
@@ -1,20 +1,13 @@
-{ cabal2nix, pkgconfig, wrap }:
+{ haskellPackages, pkgconfig, wrap }:
 
 with builtins;
 wrap {
-  name  = "hsConfig";
-  paths = [ cabal2nix pkgconfig ];
-  vars  = {
-    command = "cabal configure -v --enable-tests --enable-benchmarks";
-    expr    = ''with import <nixpkgs> {}; { c2n }:
-                (tincify (haskellPackages.callPackage c2n {}) {}).env'';
-  };
+  name   = "hsConfig";
+  paths  = [ haskellPackages.cabal-install pkgconfig ];
   script = ''
     #!/usr/bin/env bash
     set -e
-    C2N=$(cabal2nix ./.)
-
-    # shellcheck disable=SC2154
-    nix-shell --show-trace --arg c2n "$C2N" -E "$expr" --run "$command $*"
+    [[ -d .cabal-sandbox ]] || cabal sandbox init
+    cabal new-configure -v --enable-tests --enable-benchmarks "$@"
   '';
 }
