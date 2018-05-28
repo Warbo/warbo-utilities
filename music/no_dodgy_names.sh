816029513daf331abeb69d8120df3f09b5aa35ca
Chris Warburton <chriswarbo@gmail.com>
2018-05-28 20:54:13 +0100

    Speed up no_dodgy_names.sh
    Rather than running 'find' multiple times, we now:
    
     - Do *one* loop, checking against a bunch of cases for each name
     - Read in the file list from stdin, which lets us run 'find' over SSH

--- music/no_dodgy_names.sh
+++ music/no_dodgy_names.sh
@@ -6,43 +6,42 @@ function esc {
     sed -e "s@'@'\\\\''@g"
 }
 
-find Music/Commercial -name 'http___music.download.com' | while read -r DIR
+printf 'Checking stdin for files with dodgy names' 1>&2
+while read -r NAME
 do
-    PARENT=$(dirname "$DIR")
-    for ENTRY in "$DIR"/*
-    do
-         SRC=$(echo "$ENTRY"  | esc)
-        DEST=$(echo "$PARENT" | esc)
-        echo "mv '$SRC' '$DEST'"
-    done
-    ESCAPED=$(echo "$DIR" | esc)
-    echo "rmdir '$ESCAPED'"
-done
+    printf '.' 1>&2
+    case "$NAME" in
+        *http___music.download.com*)
+            PARENT=$(dirname "$NAME")
+            for ENTRY in "$DIR"/*
+            do
+                 SRC=$(echo "$ENTRY"  | esc)
+                DEST=$(echo "$PARENT" | esc)
+                echo "mv '$SRC' '$DEST'"
+            done
+            ESCAPED=$(echo "$NAME" | esc)
+            echo "rmdir '$ESCAPED'"
+            ;;
 
-find Music/Commercial -type d -name '*magnatune.com*' | while read -r DIR
-do
-     SRC=$(echo "$DIR" | esc)
-    DEST=$(echo "$DIR" | sed -e 's@ (PREVIEW_ buy it at www.magnatune.com)@@g' | esc)
-    echo "mv -v '$SRC' '$DEST'"
-done
+        *magnatune.com*)
+             SRC=$(echo "$NAME" | esc)
+            DEST=$(echo "$NAME" |
+                   sed -e 's@ (PREVIEW_ buy it at www.magnatune.com)@@g' | esc)
+            echo "mv -v '$SRC' '$DEST'"
+            ;;
 
-find Music/Commercial -type f -name '*magnatune.com*' | while read -r F
-do
-     SRC=$(echo "$F" | esc)
-    DEST=$(echo "$F" | sed -e 's@ (PREVIEW_ buy it at www.magnatune.com)@@g' | esc)
-    echo "mv -v '$SRC' '$DEST'"
-done
-
-find Music/Commercial -type f -name 'tmp_*' | while read -r F
-do
-    echo "Name '$F' looks like a dupe" 1>&2
-    OTHER=$(basename "$F" | cut -d '_' -f 2-)
-      DIR=$(dirname "$F")
-      ESC=$(echo "$F" | esc)
-    if [[ -e "$DIR/$OTHER" ]]
-    then
-        echo "rm -v '$ESC'"
-    else
-        echo "No equivalent '$OTHER' found in '$DIR' though" 1>&2
-    fi
+        *tmp_*)
+            [[ -f "$NAME" ]] || continue
+            echo -e "\nName '$NAME' looks like a dupe" 1>&2
+            OTHER=$(basename "$NAME" | cut -d '_' -f 2-)
+              DIR=$(dirname "$NAME")
+              ESC=$(echo "$NAME" | esc)
+              if [[ -e "$DIR/$OTHER" ]]
+              then
+                  echo "rm -v '$ESC'"
+              else
+                  echo -e "\nNo equivalent '$OTHER' found in '$DIR' though" 1>&2
+              fi
+              ;;
+    esac
 done
