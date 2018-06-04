78620117b6705dc7c285c316159e7e7eaa264b32
Chris Warburton <chriswarbo@gmail.com>
2018-06-04 08:48:19 +0100

    Move unfreeze_emacs to scripts/
    Removes its use of nix-shell, but we don't yet provide it with shell/pipeline

---
+++ scripts/unfreeze_emacs.nix
@@ -0,0 +1,17 @@
+{ racket, wrap, xclip }:
+
+wrap {
+  name   = "unfreeze_emacs";
+  paths  = [ racket xclip ];
+  script = ''
+    #!/usr/bin/env racket
+    #lang racket
+
+    (require shell/pipeline)
+
+    (run-pipeline '(echo "(setf debug-on-quit nil)")
+                  '(xclip))
+
+    (run-pipeline '(killall -s USR2 emacs))
+  '';
+}
