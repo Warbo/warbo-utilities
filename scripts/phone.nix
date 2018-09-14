49d048f7b7323365751707a59ca843106de256b7
Chris Warburton <chriswarbo@gmail.com>
2018-09-14 22:20:14 +0100

    Improve phone.nix

--- scripts/phone.nix
+++ scripts/phone.nix
@@ -10,15 +10,20 @@ wrap {
     [[ -n "$DEVICE" ]] || DEVICE="enp0s29f0u2"
     echo "Using DEVICE '$DEVICE'" 1>&2
 
+    [[ -n "$ADDRESS" ]] || ADDRESS="192.168.1.200"
+    echo "Using ADDRESS '$ADDRESS'" 1>&2
+
     # "Easy" method; unreliable
-    #sudo ip address add 192.168.0.200/24 dev "$DEVICE"
+    #sudo ip address add "$ADDRESS"/24 dev "$DEVICE"
     #sudo ip link set dev "$DEVICE" up
 
     # "Harder" method; seems to work, also forwards the Internet connection
-    sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s 192.168.0.0/24
+    # shellcheck disable=SC2001
+    ZERO=$(echo "$ADDRESS" | sed -e 's/[0-9]*$/0/g')
+    sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s "$ZERO"/24
     sudo sysctl -w net.ipv4.ip_forward=1
-    sudo ip addr add 192.168.0.200/24 dev "$DEVICE"
+    sudo ip addr add "$ADDRESS"/24 dev "$DEVICE"
 
-    echo "Phone should now be available at 192.168.0.202" 1>&2
+    echo "Phone should now be available at 192.168.1.202" 1>&2
   '';
 }
