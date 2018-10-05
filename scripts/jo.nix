153c9a69d3f448e3cccb3ce551756a0b47fb26f5
Chris Warburton <chriswarbo@gmail.com>
2018-10-05 12:12:35 +0100

    Don't conflate local DISPLAY with remote

--- scripts/jo.nix
+++ scripts/jo.nix
@@ -7,8 +7,8 @@ wrap {
     #!/usr/bin/env bash
     export XAUTHORITY=/home/chris/.Xauthority
 
-    [[ -n "$DISPLAY" ]] || DISPLAY=:0
-    CMD="export XAUTHORITY=/home/jo/.Xauthority; x2x -west -to $DISPLAY"
+    [[ -n "$REMOTE_DISPLAY" ]] || REMOTE_DISPLAY=:1
+    CMD="export XAUTHORITY=/home/jo/.Xauthority; x2x -west -to $REMOTE_DISPLAY"
 
     MACHINE=$(johost) || exit 1
 
