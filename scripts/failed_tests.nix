275f2002eb2c04973c66c9907d76560ce6e19575
Chris Warburton <chriswarbo@gmail.com>
2019-01-07 16:10:29 +0000

    Update failed_tests

--- scripts/failed_tests.nix
+++ scripts/failed_tests.nix
@@ -1,68 +1,23 @@
-{ bash, findutils, jq, wrap }:
+{ bash, wrap }:
 
 wrap {
   name   = "failed_tests";
-  paths  = [ bash findutils jq ];
+  paths  = [ bash ];
   script = ''
     #!/usr/bin/env bash
+    set -e
     shopt -s nullglob
 
-    D="$HOME/System/Tests"
+    resultDir="/tmp/test_results.individual"
 
-    PTHS=$(cat "$D/results/attrs.json")
+    [[ -d "$resultDir" ]] || {
+      echo "WARNING: Couldn't find failures since '$resultDir' isn't a dir" 1>&2
+      exit 0
+    }
 
-        ALL=0
-    SUCCESS=0
-       DIFF=0
-    while read -r PTH
-    do
-      ALL=$(( ALL + 1 ))
-      if [[ -f "$D/results/pass/$PTH" ]]
-      then
-        SUCCESS=$(( SUCCESS + 1 ))
-      else
-        DIFF=$(( DIFF + 1 ))
-      fi
-    done < <(echo "$PTHS")
-
-    HEX="#00FF00"
-    [[ "$DIFF" -gt 0 ]] && HEX="#FF0000"
-
-    printf '<fc=%s>%s/%s</fc>\n' "$HEX" "$SUCCESS" "$ALL"
-
-    [[ "x$1" = "xshow" ]] || exit 0
-
-    echo
-    echo "Passed:"
-    while read -r PTH
-    do
-      if [[ -f "$D/results/pass/$PTH" ]]
-      then
-        echo "$PTH"
-      fi
-    done < <(echo "$PTHS")
-
-    echo
-    echo "Not run:"
-    while read -r PTH
-    do
-      if [[ -f "$D/results/pass/$PTH" ]] ||
-         [[ -f "$D/results/fail/$PTH" ]]
-      then
-        continue
-      else
-        echo "$PTH"
-      fi
-    done < <(echo "$PTHS")
-
-    echo
-    echo "Failures:"
-    while read -r PTH
-    do
-      if [[ -f "$D/results/fail/$PTH" ]]
-      then
-        echo "$PTH"
-      fi
-    done < <(echo "$PTHS")
+    pushd "$resultDir" > /dev/null
+      grep -R 'FAIL' . | cut -d ':' -f1 | sed -e 's@^./@@g'
+    popd > /dev/null
+    exit 0
   '';
 }
