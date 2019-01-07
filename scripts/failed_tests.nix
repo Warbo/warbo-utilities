6ac09d2f206e222526a6a5d20c694313e101fc54
Chris Warburton <chriswarbo@gmail.com>
2019-01-07 13:50:05 +0000

    Refactor failed_tests to use wrap

--- scripts/failed_tests.nix
+++ scripts/failed_tests.nix
@@ -1,74 +1,68 @@
-{ bash, findutils, jq, makeWrapper, runCommand, writeScript }:
+{ bash, findutils, jq, wrap }:
 
-with {
+wrap {
+  name   = "failed_tests";
+  paths  = [ bash findutils jq ];
+  script = ''
+    #!/usr/bin/env bash
+    shopt -s nullglob
 
-script = writeScript "failed_tests" ''
-  #!${bash}/bin/bash
-  shopt -s nullglob
+    D="$HOME/System/Tests"
 
-  D="$HOME/System/Tests"
+    PTHS=$(cat "$D/results/attrs.json")
 
-  PTHS=$(cat "$D/results/attrs.json")
+        ALL=0
+    SUCCESS=0
+       DIFF=0
+    while read -r PTH
+    do
+      ALL=$(( ALL + 1 ))
+      if [[ -f "$D/results/pass/$PTH" ]]
+      then
+        SUCCESS=$(( SUCCESS + 1 ))
+      else
+        DIFF=$(( DIFF + 1 ))
+      fi
+    done < <(echo "$PTHS")
 
-      ALL=0
-  SUCCESS=0
-     DIFF=0
-  while read -r PTH
-  do
-    ALL=$(( ALL + 1 ))
-    if [[ -f "$D/results/pass/$PTH" ]]
-    then
-      SUCCESS=$(( SUCCESS + 1 ))
-    else
-      DIFF=$(( DIFF + 1 ))
-    fi
-  done < <(echo "$PTHS")
+    HEX="#00FF00"
+    [[ "$DIFF" -gt 0 ]] && HEX="#FF0000"
 
-  HEX="#00FF00"
-  [[ "$DIFF" -gt 0 ]] && HEX="#FF0000"
+    printf '<fc=%s>%s/%s</fc>\n' "$HEX" "$SUCCESS" "$ALL"
 
-  printf '<fc=%s>%s/%s</fc>\n' "$HEX" "$SUCCESS" "$ALL"
+    [[ "x$1" = "xshow" ]] || exit 0
 
-  [[ "x$1" = "xshow" ]] || exit 0
+    echo
+    echo "Passed:"
+    while read -r PTH
+    do
+      if [[ -f "$D/results/pass/$PTH" ]]
+      then
+        echo "$PTH"
+      fi
+    done < <(echo "$PTHS")
 
-  echo
-  echo "Passed:"
-  while read -r PTH
-  do
-    if [[ -f "$D/results/pass/$PTH" ]]
-    then
-      echo "$PTH"
-    fi
-  done < <(echo "$PTHS")
+    echo
+    echo "Not run:"
+    while read -r PTH
+    do
+      if [[ -f "$D/results/pass/$PTH" ]] ||
+         [[ -f "$D/results/fail/$PTH" ]]
+      then
+        continue
+      else
+        echo "$PTH"
+      fi
+    done < <(echo "$PTHS")
 
-  echo
-  echo "Not run:"
-  while read -r PTH
-  do
-    if [[ -f "$D/results/pass/$PTH" ]] ||
-       [[ -f "$D/results/fail/$PTH" ]]
-    then
-      continue
-    else
-      echo "$PTH"
-    fi
-  done < <(echo "$PTHS")
-
-  echo
-  echo "Failures:"
-  while read -r PTH
-  do
-    if [[ -f "$D/results/fail/$PTH" ]]
-    then
-      echo "$PTH"
-    fi
-  done < <(echo "$PTHS")
-'';
-
-};
-
-runCommand "failed_tests_wrapped" { buildInputs = [ makeWrapper ]; } ''
-  #!${bash}/bin/bash
-  makeWrapper "${script}" "$out" --prefix PATH : "${jq}/bin" \
-                                 --prefix PATH : "${findutils}/bin" \
-''
+    echo
+    echo "Failures:"
+    while read -r PTH
+    do
+      if [[ -f "$D/results/fail/$PTH" ]]
+      then
+        echo "$PTH"
+      fi
+    done < <(echo "$PTHS")
+  '';
+}
