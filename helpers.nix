ce7b333ac61370d0c4a4251cacd511ffb6a88995
Chris Warburton <chriswarbo@gmail.com>
2019-04-15 20:43:44 +0100

    Bump packages for green tests

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "585a7e2";
-    sha256 = "00y4ikvbs8kcrx96vl6l5szrdjkzc1j1ay184s50gkyai9b2gapq";
+    rev    = "9cb537e";
+    sha256 = "1df0453bq1ckj1cws3mwjsszpvmv1syxrqwkhnj5if7zs39618fs";
   };
 }
