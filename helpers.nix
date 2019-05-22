e6e26b0515f327e3ff9326ef8c322bec5ef8658c
Chris Warburton <chriswarbo@gmail.com>
2019-05-22 18:32:14 +0100

    Bump for asBashArray

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "48d4563";
-    sha256 = "122k4ag8c8b4ly7df57j4rihl7gghyb4sxl6y0fxyjgxda9rrgcg";
+    rev    = "1d9ce7d";
+    sha256 = "0viz492xf45md0wchfs82a5y8v5kx903b4yvi9lzdqpfdy4qg9qr";
   };
 }
