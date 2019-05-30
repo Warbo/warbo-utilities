8e10d762bda13843240101b73e79bd2f41d191de
Chris Warburton <chriswarbo@gmail.com>
2019-05-30 01:12:13 +0100

    Move 'derivation.nix' into 'overlay.nix'

--- overlay.nix
+++ overlay.nix
@@ -1,8 +1,91 @@
-self: super:
-with {
-  drvs = self.callPackage ./derivation.nix {};
-};
+with { helpers = import ./helpers.nix {}; };
+
 {
-  warbo-utilities         = drvs.pkg;
-  warbo-utilities-scripts = drvs.bin;
+  haskellPackages,
+  lib,
+  makeWrapper,
+  newScope,
+  nix-helpers    ? import helpers.nix-helpers,
+  runCommand,
+  warbo-packages ? import helpers.warbo-packages
+}:
+
+with builtins;
+with lib;
+with {
+  inherit (nix-helpers) attrsToDirs dirsToAttrs fail nixFilesIn withDeps;
+};
+rec {
+  # Let scripts depend on each other by adding 'bin' to the argument set
+  extraArgs = bin // nix-helpers // warbo-packages // {
+    raw = dirsToAttrs ./raw;
+
+    # Force xidel version, to avoid argument incompatibilities
+    inherit (nix-helpers.nixpkgs1709) xidel;
+  };
+
+  scripts = mapAttrs (_: f: newScope extraArgs f {})
+                     (nixFilesIn ./scripts);
+
+  cmds = foldl (rest: dir: rest // mapAttrs (f: _: dir + "/${f}")
+                                            (readDir dir))
+               {}
+               [ ./system ./web ./git ./development ./docs ];
+
+  bin = cmds // scripts;
+
+  check = mapAttrs (name: script: runCommand "check-${name}"
+                     {
+                       inherit script;
+                       buildInputs = [ fail haskellPackages.ShellCheck ];
+                       LANG        = "en_US.UTF-8";
+                     }
+                     ''
+                       set -e
+
+                       # Unwrap until we get to the real implementation
+                       while grep "extraFlagsArray" < "$script" > /dev/null
+                       do
+                         script=$(grep '^exec' < "$script" | cut -d ' ' -f2 |
+                                                             tr -d '"')
+                       done
+                       echo "Checking '$script'" 1>&2
+
+                       SHEBANG=$(head -n1 < "$script")
+                       echo "$SHEBANG" | grep '^#!' > /dev/null || {
+                         # Binaries, etc.
+                         mkdir "$out"
+                         exit 0
+                       }
+
+                       echo "$SHEBANG" | grep 'usr/bin/env' > /dev/null ||
+                       echo "$SHEBANG" | grep '/nix/store'  > /dev/null ||
+                         fail "Didn't use /usr/bin/env or /nix/store:\n$SHEBANG"
+
+                       if echo "$SHEBANG" | grep 'bash' > /dev/null
+                       then
+                         shellcheck "$script"
+                       fi
+                       mkdir "$out"
+                     '')
+                   bin;
+
+  pkg = withDeps (attrValues check)
+                 (runCommand "warbo-utilities"
+                   {
+                     bin         = attrsToDirs bin;
+                     buildInputs = [ makeWrapper ];
+                   }
+                   ''
+                     echo "Tying the knot between scripts" 1>&2
+                     mkdir -p "$out/bin"
+                     for P in "$bin"/*
+                     do
+                       F=$(readlink -f "$P")
+                       N=$(basename    "$P")
+                       cp "$F"  "$out/bin/$N"
+                       chmod +x "$out/bin/$N"
+                       wrapProgram "$out/bin/$N" --prefix PATH : "$out/bin"
+                     done
+                   '');
 }
