c4df2caa19de2b410a84ad052e96f3529b896233
Chris Warburton <chriswarbo@gmail.com>
2019-06-13 18:36:39 +0100

    Bamp

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "ec2de3f";
-    sha256 = "0g4bkigbins74qsga6ajkb97c75lkp7rskdkzqqdms0csm0g97b4";
+    rev    = "3157719";
+    sha256 = "1zayfrrnqjhl1l57mfb31pn943b2zvjgl6p3kvz6wslg0r0gd6wi";
   };
 }
