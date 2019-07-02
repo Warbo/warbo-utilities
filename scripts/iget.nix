90ff43bec6ecaf873062d319d5f3dc3fc962e0b1
Chris Warburton <chriswarbo@gmail.com>
2019-07-02 15:22:29 +0100

    Add deps to iget

--- scripts/iget.nix
+++ scripts/iget.nix
@@ -1,7 +1,8 @@
-{ wrap }:
+{ bash, get_iplayer, wrap }:
 
 wrap {
   name   = "iget";
+  paths  = [ bash get_iplayer ];
   script = ''
     #!/usr/bin/env bash
     get_iplayer --no-purge --modes=good --raw "$@"
