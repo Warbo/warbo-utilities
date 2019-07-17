6482200513780cf3b82c3e30c0b2b2182ade5e99
Chris Warburton <chriswarbo@gmail.com>
2019-07-17 11:21:40 +0100

    Bump for iplayer fix

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "600ab59";
-    sha256 = "0a3j2r9rqlpr4g6iml7zbd2498qrj2dlh7vib6w8mpxig3270z1h";
+    rev    = "3352051";
+    sha256 = "00mljs63sbpc3np6vzbgrk45qy5mqq28692644id8d9plaikkjrz";
   };
 }
