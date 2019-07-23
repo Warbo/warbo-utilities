1cc321845479b3a132eec5f96ae64b765f367eda
Chris Warburton <chriswarbo@gmail.com>
2019-07-23 19:58:26 +0100

    Bump for newer firefox

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "3352051";
-    sha256 = "00mljs63sbpc3np6vzbgrk45qy5mqq28692644id8d9plaikkjrz";
+    rev    = "f0a0ce0";
+    sha256 = "1c0zqpvi758p5vl031cv23iwr9z309n3rlw9nc06iwrxp2wwdi03";
   };
 }
