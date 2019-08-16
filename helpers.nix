9680325957d4d0b2d40a77316577387c4df70cbe
Chris Warburton <chriswarbo@gmail.com>
2019-08-16 11:13:52 +0100

    Bump for checkedRacket

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "ea2e64e";
-    sha256 = "1kz0d567m45745fzcp3flbhkcr05diig67scdhsdq3qxm9sh502s";
+    rev    = "762452e";
+    sha256 = "1cw5g2ip2lr3mq2png216v7x7kkp4m3ypnbj992cb98mkv4sa1fr";
   };
 }
