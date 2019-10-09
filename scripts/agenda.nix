3a73261194efc30dce1aa7d27659a67411184cb8
Chris Warburton <chriswarbo@gmail.com>
2019-10-09 15:59:48 +0100

    Use wrap for agenda

--- scripts/agenda.nix
+++ scripts/agenda.nix
@@ -1,34 +1,26 @@
-{ bash, coreutils, gcalcli, gnugrep, gnused, makeWrapper, runCommand,
-  writeScript }:
+{ bash, coreutils, gcalcli, gnugrep, gnused, wrap }:
 
-runCommand "mk-agenda"
-  {
-    buildInputs = [ makeWrapper ];
-    raw = writeScript "agenda" ''
-      #!${bash}/bin/bash
+wrap {
+  name   = "agenda";
+  paths  = [ bash coreutils gcalcli gnugrep gnused ];
+  script = ''
+    #!/usr/bin/env bash
 
-      # Start agenda from half an hour ago
-         NOW=$(date "+%s")
-       DELTA=$((60 * 30))
-        THEN=$((NOW - DELTA))
-        DATE=$(date -d "@$THEN" "+%Y-%m-%dT%H:%M")
-      RESULT=$(gcalcli --military --nocolor agenda "$DATE")
-        CODE=$?
+    # Start agenda from half an hour ago
+       NOW=$(date "+%s")
+     DELTA=$((60 * 30))
+      THEN=$((NOW - DELTA))
+      DATE=$(date -d "@$THEN" "+%Y-%m-%dT%H:%M")
+    RESULT=$(gcalcli --military --nocolor agenda "$DATE")
+      CODE=$?
 
-      if [[ "x$1" = "xhead" ]]
-      then
-        echo "$RESULT" | grep '[0-9]' | head -n1 | sed -e 's/  */ /g'
-      else
-        echo "$RESULT"
-      fi
+    if [[ "x$1" = "xhead" ]]
+    then
+      echo "$RESULT" | grep '[0-9]' | head -n1 | sed -e 's/  */ /g'
+    else
+      echo "$RESULT"
+    fi
 
-      exit "$CODE"
-    '';
-  }
-  ''
-    #!${bash}/bin/bash
-    makeWrapper "$raw" "$out" --prefix PATH : "${coreutils}/bin" \
-                              --prefix PATH : "${gcalcli}/bin"   \
-                              --prefix PATH : "${gnugrep}/bin"   \
-                              --prefix PATH : "${gnused}/bin"
-  ''
+    exit "$CODE"
+  '';
+}
