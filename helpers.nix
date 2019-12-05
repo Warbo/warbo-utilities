9c96161151f704b5d8e344a3cf7af9f8c211a090
Chris Warburton <chriswarbo@gmail.com>
2019-12-05 18:18:07 +0000

    Bump

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "6626cc0";
-    sha256 = "1pdrxcq5r1pqyijd4216fgs3663wsi07js5cfq4bnh53qv687rkf";
+    rev    = "9ba9675";
+    sha256 = "0mlcjji4hk46jf561k46kw5w0b6dwaigg77gg48qsxyw22267x4f";
   };
 }
