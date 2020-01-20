e6a4d96b5aa35809150377e31aac8c181e4aa53f
Chris Warburton <chriswarbo@gmail.com>
2020-01-20 18:05:48 +0000

    Provide shell/pipeline library for Racket

--- scripts/unfreeze_emacs.nix
+++ scripts/unfreeze_emacs.nix
@@ -1,14 +1,26 @@
-{ checkedRacket, wrap, xclip }:
+{ checkedRacket, fetchFromGitHub, racketWithPackages, wrap, xclip }:
 
+with {
+  racket = racketWithPackages.override { racket = checkedRacket; } [
+    (fetchFromGitHub {
+      owner  = "willghatch";
+      repo   = "racket-shell-pipeline";
+      rev    = "7ed9a75";
+      sha256 = "06z5bhmvpdhy4bakh30fzha4s0xp2arjq8h9cyi65b1y18cd148x";
+    })
+  ];
+};
 wrap {
   name   = "unfreeze_emacs";
-  paths  = [ checkedRacket xclip ];
+  paths  = [ racket xclip ];
   script = ''
-    #!${checkedRacket}/bin/racket
+    #!${racket}/bin/racket
     #lang racket
-
     (require shell/pipeline)
 
+    ;; If emacs does become responsive, debug-on-quit will be set, which spawns
+    ;; annoying buffers whenever C-g is pressed (which is a lot!). We put the
+    ;; elisp for disabling it into the clipboard, in case we want it.
     (run-pipeline '(echo "(setf debug-on-quit nil)")
                   '(xclip))
 
