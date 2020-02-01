ec355e727406c60dbbdaacf307ed4e633e52105c
Chris Warburton <chriswarbo@gmail.com>
2020-02-01 09:06:18 +0000

    Bump
    Upgrades jsbeautifier, which should fix our build on NixOS 10.09

--- helpers.nix
+++ helpers.nix
@@ -6,7 +6,7 @@ rec {
 
   warbo-packages = fetchgit {
     url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "a15a328";
-    sha256 = "03zp1sx18m244sgv0k0wp3zdp0pk9yf5yvhp08iwcwnqx9lc0gyd";
+    rev    = "6884dc9";
+    sha256 = "0vchy16bbg3sj0b4yarzbh56ynl8l3prwfijhrax4vznizwnfbgw";
   };
 }
