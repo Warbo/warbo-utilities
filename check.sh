812c17ce4ece5a016988ea2132335431cdf0815d
Chris Warburton <chriswarbo@gmail.com>
2020-02-03 17:55:41 +0000

    Only check Nix syntax of files

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Simple, quick sanity check. Useful as a git pre-commit hook.
-find . -name "*.nix" | while read -r F
+find . -name "*.nix" -type f | while read -r F
 do
     echo "Checking '$F'" 1>&2
     nix-instantiate --parse "$F" > /dev/null
