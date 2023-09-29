5906a2b7c2b9b4d3c4d323b5bc11a71df3d2dad1
Chris Warburton <chriswarbo@gmail.com>
2023-09-29 15:43:41 +0100

    fix-leveldb: not general enough to keep

--- raw/fix-leveldb.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import leveldb
-import sys
-if len(sys.argv) < 2: raise Exception('Please give leveldb dir as argument')
-db = sys.argv[1]
-sys.stderr.write('Attempting to repair leveldb at {0}\n'.format(db))
-leveldb.RepairDB(db)
-sys.stderr.write('Finished processing {0}\n'.format(db))
