63d4d7b2eb6eff40ab93c2dc06bab69a82c0dffe
Chris Warburton <chriswarbo@gmail.com>
2023-09-30 00:30:53 +0100

    Move overlays.nix definitions into default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,101 @@
 { helpers ? import ./helpers.nix, nix-helpers ? warbo-packages.nix-helpers
-, sources ? helpers.sources, warbo-packages ? helpers.warbo-packages }:
+, nixpkgs ? nix-helpers.nixpkgs, nixpkgs-lib ? nix-helpers.nixpkgs-lib
+, warbo-packages ? helpers.warbo-packages }:
 
 with rec {
-  warbo-packages-sources = import "${sources.warbo-packages}/nix/sources.nix";
+  inherit (builtins) attrValues elem foldl isAttrs mapAttrs readDir substring;
 
-  pkgs = import nix-helpers.repoLatest {
-    config = { };
-    overlays = [
-      (import "${warbo-packages-sources.nix-helpers}/overlay.nix")
-      (import "${sources.warbo-packages}/overlay.nix")
-      (import ./overlay.nix)
-    ];
+  inherit (nix-helpers.nixpkgs-lib) escapeShellArg;
+
+  inherit (nix-helpers)
+    attrsToDirs dirsToAttrs fail foldAttrs' nixFilesIn nixpkgs1709 patchShebang
+    withDeps xvfb-run-safe;
+
+  inherit (nixpkgs) makeWrapper newScope runCommand shellcheck;
+
+  # Let scripts depend on each other by adding 'bin' to the argument set
+  extraArgs = {
+    raw = mapAttrs (name: entry:
+      if isAttrs entry then
+        ./raw + "/${name}"
+      else if elem name [ "alert.wav" "bbcExamplePage.html.gz" ] then
+        entry
+      else
+        patchShebang {
+          inherit name;
+          file = entry;
+        }) (dirsToAttrs ./raw);
+
+    scripts = warbo-utilities-scripts;
+
+    # Force xidel version, to avoid argument incompatibilities
+    inherit (nixpkgs1709) xidel;
   };
+
+  scripts = mapAttrs (_: f: newScope extraArgs f { }) (nixFilesIn ./scripts);
+
+  cmds = foldl (rest: dir: rest // mapAttrs (f: _: dir + "/${f}") (readDir dir))
+    { } [ ./system ./web ./git ./development ./docs ];
+
+  check = mapAttrs (name: script:
+    runCommand "check-${name}" {
+      inherit script;
+      buildInputs = [ fail shellcheck ];
+      LANG = "en_US.UTF-8";
+    } ''
+      set -e
+
+      # Unwrap until we get to the real implementation
+      while grep "extraFlagsArray" < "$script" > /dev/null
+      do
+        script=$(grep '^exec' < "$script" | cut -d ' ' -f2 |
+                                            tr -d '"')
+      done
+      echo "Checking '$script'" 1>&2
+
+      SHEBANG=$(head -n1 < "$script")
+      echo "$SHEBANG" | grep '^#!' > /dev/null || {
+        # Binaries, etc.
+        mkdir "$out"
+        exit 0
+      }
+
+      echo "$SHEBANG" | grep 'usr/bin/env' > /dev/null ||
+      echo "$SHEBANG" | grep '/nix/store'  > /dev/null ||
+        fail "Didn't use /usr/bin/env or /nix/store:\n$SHEBANG"
+
+      if echo "$SHEBANG" | grep 'bash' > /dev/null
+      then
+        shellcheck "$script"
+      fi
+      mkdir "$out"
+    '') warbo-utilities-scripts;
+
+  warbo-utilities-scripts = cmds // scripts;
 };
-pkgs.warbo-utilities
+withDeps (attrValues check) (runCommand "warbo-utilities" {
+  __noChroot = true; # To access files linked to by our deps
+  bin = attrsToDirs warbo-utilities-scripts;
+  buildInputs = [ fail makeWrapper ];
+  forContext = foldAttrs' (_: val: str:
+    substring 0 0 ''
+      ${val} ${str}
+    '') "" warbo-utilities-scripts;
+} ''
+  echo "Tying the knot between scripts" 1>&2
+  mkdir -p "$out/bin" || fail "Couldn't make '$out/bin'"
+  for P in ${escapeShellArg fail}'/bin/fail'          \
+           ${escapeShellArg xvfb-run-safe}'/bin/xvfb-run-safe' \
+           "$bin"/*
+  do
+    F=$(readlink -f "$P") || fail "Couldn't readlink '$P'"
+    N=$(basename    "$P") || fail "No basename for '$P'"
+    cp    "$F" "$out/bin/$N" || fail "Copy failed for '$F'"
+    chmod +x   "$out/bin/$N" || fail "Couldn't chmod for '$F'"
+    wrapProgram "$out/bin/$N" --prefix PATH : "$out/bin" ||
+      fail "Couldn't wrap '$P'"
+  done
+  echo "Finished wrapping scripts" 1>&2
+'') // {
+  inherit cmds scripts;
+}
