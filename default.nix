3966b694cbf5fa7b36d5ded2b9e3b14e8f302007
Chris Warburton <chriswarbo@gmail.com>
2023-09-29 15:53:24 +0100

    Turn release.nix definitions into default.nix default arguments

--- default.nix
+++ default.nix
@@ -1,1 +1,16 @@
-(import ./release.nix).warbo-utilities
+{ helpers ? import ./helpers.nix, nix-helpers ? helpers.nix-helpers
+, sources ? helpers.sources, warbo-packages ? helpers.warbo-packages }:
+
+with rec {
+  warbo-packages-sources = import "${sources.warbo-packages}/nix/sources.nix";
+
+  pkgs = import nix-helpers.repoLatest {
+    config = { };
+    overlays = [
+      (import "${warbo-packages-sources.nix-helpers}/overlay.nix")
+      (import "${sources.warbo-packages}/overlay.nix")
+      (import ./overlay.nix)
+    ];
+  };
+};
+pkgs.warbo-utilities
