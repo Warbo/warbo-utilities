230899ce4f94e784233ca52aa73e28d0a9b3acc3
Chris Warburton <chriswarbo@gmail.com>
2023-09-30 17:14:49 +0100

    Bump warbo-packages to avoid python2 packages

--- warbo-packages.nix
+++ warbo-packages.nix
@@ -2,5 +2,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "6f36f4d1ec8b56151fa958da114358bc882f28d3";
+  rev = "f0b70959da15947c07a45b689343403cce5e9950";
 }) { }
