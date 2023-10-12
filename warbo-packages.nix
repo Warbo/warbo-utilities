4176ae9bfe0a026be3ee17a18a5a1b2bf66a31c4
Chris Warburton <chriswarbo@gmail.com>
2023-10-12 21:54:59 +0100

    warbo-packages: bump for suffixedFilesIn

--- warbo-packages.nix
+++ warbo-packages.nix
@@ -2,5 +2,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "f0b70959da15947c07a45b689343403cce5e9950";
+  rev = "8a318cb1cbfc5dd8bc8c07e8abb018f89d02db44";
 }) { }
