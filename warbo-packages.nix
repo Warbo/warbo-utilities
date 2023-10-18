cd2b071ff22fcbbd80163e5ffafc39ad3955fd42
Chris Warburton <chriswarbo@gmail.com>
2023-10-18 03:13:47 +0100

    warbo-packages: Bump for Artemis fixes

--- warbo-packages.nix
+++ warbo-packages.nix
@@ -2,5 +2,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "8a318cb1cbfc5dd8bc8c07e8abb018f89d02db44";
+  rev = "11d53defbb94552d325d0f8e3fe2ef7807d8f680";
 }) { }
