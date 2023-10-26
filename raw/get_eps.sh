0bf925e70f8e182b99f254a29e155d4cb367dbee
Chris Warburton <chriswarbo@gmail.com>
2023-10-26 02:11:13 +0100

    get_eps: move Bash code to raw file

---
+++ raw/get_eps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -e "$2" ]]
+then
+    PAGE=$(cat "$2")
+else
+    echo "$2" | grep 'epguides.com' > /dev/null ||
+        fail 'get_eps URL should be from epguides.com'
+
+    PAGE=$(curl -f "$2") || fail "Couldn't download '$2'"
+fi
+
+# shellcheck disable=SC2154
+echo "$PAGE" | FEED="$1" "$processor"
--- scripts/get_eps.nix
+++ scripts/get_eps.nix
@@ -15,28 +15,12 @@ with rec {
 
   go = wrap {
     name = "get-eps";
+    file = raw."get_eps.sh";
     paths = [ bash coreutils curl glibc.bin wget ];
     vars = {
       inherit processor;
       SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
     };
-    script = ''
-      #!${bash}/bin/bash
-      set -e
-
-      if [[ -e "$2" ]]
-      then
-        PAGE=$(cat "$2")
-      else
-        echo "$2" | grep 'epguides.com' > /dev/null ||
-          fail 'get_eps URL should be from epguides.com'
-
-        PAGE=$(curl -f "$2") || fail "Couldn't download '$2'"
-      fi
-
-      # shellcheck disable=SC2154
-      echo "$PAGE" | FEED="$1" "$processor"
-    '';
   };
 
   tests = {
