4c229ef0643f1dd497d45046e7ef728339e3f2fd
Chris Warburton <chriswarbo@gmail.com>
2023-10-26 01:42:19 +0100

    checkTV: Take SSL certificates from cacert package

--- scripts/checkTV.nix
+++ scripts/checkTV.nix
@@ -1,8 +1,8 @@
-{ bash, openssl, wget, wrap }:
+{ bash, cacert, openssl, raw, wget, wrap }:
 
 wrap {
   name = "checkTV";
-  vars = { SSL_CERT_FILE = /etc/ssl/certs/ca-bundle.crt; };
+  vars = { SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt"; };
   paths = [ bash openssl (openssl.dev or openssl) wget ];
   script = ''
     #!${bash}/bin/bash
