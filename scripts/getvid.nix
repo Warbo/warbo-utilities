da232a8ddbc413b5f4ee455824da87c6b51beb08
Chris Warburton <chriswarbo@gmail.com>
2023-10-26 01:59:09 +0100

    getvid: move Bash code to raw file

--- scripts/getvid.nix
+++ scripts/getvid.nix
@@ -57,6 +57,7 @@ with rec {
 };
 wrap {
   name = "getvid";
+  file = raw."getvid.sh";
   paths = [ bash xidel ];
   vars = {
     inherit f5 voza vse;
@@ -76,73 +77,4 @@ wrap {
         ${vse}
     '';
   };
-  script = ''
-    #!${bash}/bin/bash
-    set -e
-
-    if [[ "$1" = "--help" ]]
-    then
-      # shellcheck disable=SC2154
-      echo "$msg" 1>&2
-      exit 0
-    fi
-
-    echo "Run with --help as the only arg to see usage and handler scripts" 1>&2
-
-    function esc {
-      # shellcheck disable=SC1003
-      sed -e "s/'/'"'\\'"'''/g"
-    }
-
-    # shellcheck disable=SC2154
-    LINKS=$("$list" "$@")
-
-    echo "LINKS: $LINKS" 1>&2
-
-    function tryScrape {
-         LINK="$1"
-          TIT="$2"
-        REGEX="$3"
-      SCRAPER="$4"
-
-      echo "$LINK" | grep "$REGEX" > /dev/null || return 1
-
-      [[ -n "$DEBUG" ]] && echo "Running $SCRAPER on $LINK" 1>&2
-      URL=$("$SCRAPER" "$LINK") || return 0
-
-      [[ -n "$URL" ]] || return 0
-      URL=$(echo "$URL" | esc)
-
-      echo "wget --no-check-certificate -c -O '$TIT' '$URL'"
-      return 0
-    }
-
-    echo "$LINKS" | while read -r PAIR
-    do
-      LINK=$(echo "$PAIR" | cut -f2)
-      [[ -n "$LINK" ]] || continue
-
-      TITLE=$(echo "$PAIR" | cut -f1 | esc)
-      [[ -n "$TITLE" ]] || TITLE="UNKNOWN"
-
-      [[ -n "$DEBUG" ]] && echo "Checking $LINK" 1>&2
-
-      # Try "simulating" a youtube-dl run; if it works, present it as an option
-      if youtube-dl --no-check-certificate -s "$LINK" > /dev/null 2>&1
-      then
-        URL=$(echo "$LINK" | esc)
-        echo "youtube-dl --no-check-certificate --output '$TITLE' '$URL'"
-      fi
-      URL=""
-
-      # shellcheck disable=SC2154
-      tryScrape "$LINK" "$TITLE" 'x5[4-6][4-6]\.c' "$f5"   && continue
-
-      # shellcheck disable=SC2154
-      tryScrape "$LINK" "$TITLE" '/vi..z.\.net/'   "$voza" && continue
-
-      # shellcheck disable=SC2154
-      tryScrape "$LINK" "$TITLE" '/vs...e\.e'      "$vse"  && continue
-    done
-  '';
 }
