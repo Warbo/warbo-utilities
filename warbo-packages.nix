872a39b91a62f037d2557e963e28b30bd34b8c94
Chris Warburton <chriswarbo@gmail.com>
2023-10-26 11:23:34 +0100

    warbo-packages: update to avoid warning

--- warbo-packages.nix
+++ warbo-packages.nix
@@ -2,5 +2,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "11d53defbb94552d325d0f8e3fe2ef7807d8f680";
+  rev = "d0ef3f30de3b1476d368d26acdc9105ac3e3ca42";
 }) { }
