05d403e2e1ad6435a0a516fd31eb778ba184824a
Chris Warburton <chriswarbo@gmail.com>
2023-10-30 16:05:15 +0000

    yt: rename to ytdl-wrapped (it's needed after all)

--- scripts/fetch.nix
+++ scripts/fetch.nix
@@ -4,5 +4,5 @@ wrap {
   name = "fetch";
   file = raw."fetch.sh";
   paths = [ bash wget xidel ];
-  vars = { inherit (scripts) fetch_podcasts youtube_then_mark yt; };
+  vars = { inherit (scripts) fetch_podcasts youtube_then_mark; };
 }
---
+++ scripts/youtube_then_mark.nix
@@ -0,0 +1,8 @@
+{ bash, raw, scripts, wrap }:
+
+wrap {
+  name = "youtube_then_mark";
+  file = raw."youtube_then_mark.sh";
+  paths = [ bash ];
+  vars.yt = scripts.ytdl-wrapped;
+}
