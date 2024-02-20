b77a2175f2075554a58b06e2c755d4749ad46a4f
Chris Warburton <chriswarbo@gmail.com>
2024-02-20 21:48:57 +0000

    inNixedDir: use script from nix-helpers

--- scripts/inNixedDir.nix
+++ scripts/inNixedDir.nix
@@ -1,7 +1,11 @@
-{ bash, coreutils, raw, wrap }:
+# Like inNixedDir provided by nix-helpers, but this version doesn't make
+# assumptions about what Nix env vars are supposed to be.
+# TODO: The inNixedDir in nix-helpers should probably have those env vars
+# removed, making it equivalent to this.
+{ bash, coreutils, nix-helpers, wrap }:
 wrap {
   name = "inNixedDir";
-  file = raw."inNixedDir.sh";
+  file = nix-helpers.inNixedDir.file;
   paths = [ bash coreutils ];
   vars = { };
 }
--- warbo-packages.nix
+++ warbo-packages.nix
@@ -2,5 +2,5 @@ import (builtins.fetchGit {
   name = "warbo-packages";
   url = "http://chriswarbo.net/git/warbo-packages.git";
   ref = "master";
-  rev = "729fe971ec5eb9474c9b2981ad4f30c2372e9084";
+  rev = "0a3714752c36dd8afde9276b7d5f2e054d6d4325";
 }) { }
