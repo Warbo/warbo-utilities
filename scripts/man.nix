0bf82ac5591e5ba604993e3017af230f4d6486c3
Chris Warburton <chriswarbo@gmail.com>
2025-05-11 15:45:35 +0100

    man: make less annoying

--- scripts/man.nix
+++ scripts/man.nix
@@ -6,16 +6,16 @@
 
 wrap {
   name = "man";
-  paths = [ pkgs.man ];
   script = ''
     #!${bash}/bin/bash
-    if [[ "$TERM" = "dumb" ]]
+    REAL=${pkgs.man}/bin/man
+    if [[ -n "$INSIDE_EMACS" ]]
     then
       # We're in Emacs, open this man page in Emacs's viewer
-      emacsclient -e "(man \"$1\")"
+      emacsclient -e "(let ((manual-program \"$REAL\")) (man \"$1\"))"
     else
       # We're outside Emacs, use the normal man binary
-      exec man "$@"
+      exec "$REAL" "$@"
     fi
   '';
 }
