30a2ea7d90d75a12f417033f93291da33c0b537c
Chris Warburton <chriswarbo@gmail.com>
2025-06-25 18:09:11 +0100

    aws-login: fir warbo-utilities format

--- scripts/aws-login.nix
+++ scripts/aws-login.nix
@@ -4,16 +4,20 @@
   gnused,
   jq,
   pass,
+  raw,
   writeShellApplication,
+  writeShellScript,
 }:
-writeShellApplication {
-  name = "aws-login";
-  text = builtins.readFile ./aws-login.sh;
-  runtimeInputs = [
-    coreutils
-    gnused
-    jq
-    pass
-    awscli
-  ];
-}
+"${
+  writeShellApplication {
+    name = "aws-login";
+    text = builtins.readFile raw."aws-login.sh";
+    runtimeInputs = [
+      coreutils
+      gnused
+      jq
+      pass
+      awscli
+    ];
+  }
+}/bin/aws-login"
