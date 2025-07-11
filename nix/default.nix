a40e7b564cb1e8be42badfa3e3b6b91395959aab
Chris Warburton <chriswarbo@gmail.com>
2025-07-11 23:59:19 +0100

    Bump warbo-packages for multibase

--- nix/default.nix
+++ nix/default.nix
@@ -5,7 +5,7 @@
   nixpkgs-lib ? null,
   warbo-packages ? null,
   warbo-packages-tree ? {
-    sha1 = "0827994c30afcbf2c00941425f20a75b03094435";
+    sha1 = "5d764198fa60d27d7d4b75943cbfcf6a40ce4618";
   },
 }:
 with rec {
